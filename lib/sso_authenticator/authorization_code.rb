2069b9be736f867bc6d831fd28103ac8c832f26f
adham90 <hi@adham.dev>
2024-03-06 23:22:01 +0200

    update version

--- lib/sso_authenticator/authorization_code.rb
+++ lib/sso_authenticator/authorization_code.rb
@@ -7,17 +7,17 @@ module SsoAuthenticator
       puts "Debug: Session Store URI is #{SsoAuthenticator.configuration.inspect}"
 
       uri = URI(SsoAuthenticator.configuration.sso_service_url)
-      response = Net::HTTP.post_form( uri, code: code, client_id: SsoAuthenticator.configuration.client_id, client_secret: SsoAuthenticator.configuration.client_secret )
+      response = Net::HTTP.post_form(uri, code: code, client_id: SsoAuthenticator.configuration.client_id, client_secret: SsoAuthenticator.configuration.client_secret)
       response_body = JSON.parse(response.body).symbolize_keys
       token = response_body[:token]
       raise ActiveRecord::RecordNotFound if token.nil?
-  
+
       payload = JSON.parse($session_store.get(token)).symbolize_keys
       if payload.nil?
         render json: { error: "Invalid token" }, status: :unauthorized
         return
       end
-  
+
       token
     end
   end
--- lib/sso_authenticator/controller_helpers.rb
+++ lib/sso_authenticator/controller_helpers.rb
@@ -1,17 +1,15 @@
 module SsoAuthenticator
   module ControllerHelpers
     def logged_in?
-      # Assuming SsoAuthenticator::Current.user is set in your middleware
-      # and it returns nil if no user is logged in
       SsoAuthenticator::Current.user.present?
     end
 
     def require_login
-      unless logged_in?
-        sso_service_url = SsoAuthenticator.configuration.sso_service_url
-        client_id = SsoAuthenticator.configuration.client_id
-        redirect_to "#{sso_service_url}?client_id=#{client_id}"
-      end
+      return if logged_in?
+
+      sso_service_url = SsoAuthenticator.configuration.sso_service_url
+      client_id = SsoAuthenticator.configuration.client_id
+      redirect_to "#{sso_service_url}/sso/#{client_id}"
     end
   end
 end
